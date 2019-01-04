ce7ad0684d71f3d5a0702f0645e4451eafd7bb0b
nguyenhongphat0 <hongphat.js@gmail.com>
2019-01-04 17:47:26 +0700

    Updates

---
+++ precache-manifest.c2a68cea4c53ee7488a866809f8f2dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7743ecb096be6591998",
+    "url": "/static/css/main.20c6a392.chunk.css"
+  },
+  {
+    "revision": "e7743ecb096be6591998",
+    "url": "/static/js/main.e7743ecb.chunk.js"
+  },
+  {
+    "revision": "b6fdd634e45074a4012f",
+    "url": "/static/js/1.b6fdd634.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a338998ab5b45eaef8065a83490f860c",
+    "url": "/index.html"
+  }
+];
