7f650eb453f116a99123afaaecc3b8fbb04ed5de
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2019-04-15 17:45:50 +0700

    Updates

---
+++ precache-manifest.909508fdfb28d0a269a61e6c1b3ad4df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "321af161c25d898be74c",
+    "url": "/static/js/main.321af161.chunk.js"
+  },
+  {
+    "revision": "5bb0dbdf5132810e5931",
+    "url": "/static/js/1.5bb0dbdf.chunk.js"
+  },
+  {
+    "revision": "321af161c25d898be74c",
+    "url": "/static/css/main.8ff7d699.chunk.css"
+  },
+  {
+    "revision": "4ca03f208f79a968d648610907469961",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17f3fd66072fe88cef065a6b13a7c026.js"
+  "/precache-manifest.909508fdfb28d0a269a61e6c1b3ad4df.js"
 );
 
 workbox.clientsClaim();
